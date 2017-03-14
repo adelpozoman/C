3457be3a1ba9d89582d0d3e2664f548b1313649a
adelpozoman <adelpozoman@gmail.com>
2017-03-14 19:32:45 +0100

    Add files via upload

--- Ejercicios1UPM/E16.c
+++ Ejercicios1UPM/E16.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define centiafarenh(m) ((m * (9/5)) + 32)
-#define farenhacenti(m) ((0.555555555555555555) * (m - 32))
+#define centiafarenh(m) ((m*(1.8)) + 32)
+#define farenhacenti(m) (m-32)*0.5555555556
 //ejercicio 16
 int main(void){
 	
 	char u;
-	int m; //la medida que tengamos( el nÃºmkero de grados)
-	printf("Â¿Quieres pasar de Centigrados a Farenheit o de Farenheit a Centigrados?\n Escribe c para la primera opciÃ³n y f para la segunda\n");
+	float m; //la medida que tengamos( el númkero de grados)
+	printf("¿Quieres pasar de Centigrados a Farenheit o de Farenheit a Centigrados?\n Escribe c para la primera opción y f para la segunda\n");
 	scanf("%s", &u);
 	//nos pasa de un tipo a otro, dependiendo del inicial
 	if(u == 'c'){
-		printf("Â¿Escribe un numero natural de grados centigrados\n");
-		scanf("%i", &m);
-		printf("Ese valor corresponde a %d grados Farenheit\n", centiafarenh(m));
+		printf("¿Escribe un numero natural de grados centigrados\n");
+		scanf("%f", &m);
+		printf("Ese valor corresponde a %.2f grados Farenheit\n", centiafarenh(m));
 		}
 	
 	else if (u == 'f'){
 		printf("Escribe un numero natural de grados farenheit\n");
-		scanf("%i", &m);
-		printf("Ese valor corresponde a %i grados centigrados\n", farenhacenti(m));
+		scanf("%f", &m);
+		printf("Ese valor corresponde a %.2f grados centigrados\n", farenhacenti(m));
 		}
 			
 	return EXIT_SUCCESS;
 	}
-
-
-
-
-
-
-
-
-
 
 
