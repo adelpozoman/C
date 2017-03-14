b56543de3988224ab61d7f7feb6a997b3d02a7f2
adelpozoman <adelpozoman@gmail.com>
2017-03-14 19:34:12 +0100

    quitado lo de las tildes

--- Ejercicios1UPM/E18distanciaalorigen.c
+++ Ejercicios1UPM/E18distanciaalorigen.c
@@ -5,59 +5,6 @@
 #define MODULO(x,y) sqrt((x*x)+(y*y))
 //ejercicio18
 
-void Printf(const char *strText)
-{
-char ch;
-short i,iLength;
-iLength=strlen(strText);
-
-for(i=0;i<iLength;i++){
-ch=strText[i];
-switch(ch){
-case 'á':
-printf("\240");
-break;
-case 'é':
-printf("\202");
-break;
-case 'í':
-printf("\241");
-break;
-case 'ó':
-printf("\242");
-break;
-case 'ú':
-printf("\243");
-break;
-case 'ñ':
-printf("%c",-92);
-break;
-case 'Ñ':
-printf("%c",-91);
-break;
-case 'Á':
-printf("%c",-75);
-break;
-case 'É':
-printf("%c",-112);
-break;
-case 'Í':
-printf("%c",-42);
-break;
-case 'Ó':
-printf("%c",-32);
-break;
-case 'Ú':
-printf("%c",-23);
-break;
-default:
-printf("%c",ch);
-break;
-
-}
-}
-}
-
 main(){
 	int x,y;
 	puts("Escribe las coordenadas cartesianas de un punto y te doy su distancia al origen");
