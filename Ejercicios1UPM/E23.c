e3278028a9ec855428a175222cfc2a7530922b55
adelpozoman <adelpozoman@gmail.com>
2017-03-22 18:04:07 +0100

    Agregado un contador

--- Ejercicios1UPM/E23.c
+++ Ejercicios1UPM/E23.c
@@ -3,10 +3,9 @@
 //ejercicio 23
 
 int main(){
-	
 	char c;
-	while((c=getchar())!='\n'){
-		putchar(c);
-	}
-	
-	return 0;}
+	int contador=0;
+	while((c=getchar())!='\n'){	putchar(c);
+								contador++;}
+	printf("\nHas escrito %i carácteres",contador);
+return 0;}
