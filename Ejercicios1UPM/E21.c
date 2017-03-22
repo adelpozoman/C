f5b2abc787fe168b6c550cbe5ceaa8aca0abf183
adelpozoman <adelpozoman@gmail.com>
2017-03-22 18:13:36 +0100

    limpiado

--- Ejercicios1UPM/E21.c
+++ Ejercicios1UPM/E21.c
@@ -3,16 +3,14 @@
 #include <math.h>
 #include <string.h>
 /*ejercicio21 */
-//12,15,26,...27,29,30
-
 main(){
 
 	int n,s;
-	puts("Introduce un número\n");
+	puts("Introduce un nÃºmero\n");
 	do{
-		if(scanf("%i",&n)!=1){	printf("No has ingresado un número\n");
+		if(scanf("%i",&n)!=1){	printf("No has ingresado un nÃºmero");
 								while(getchar()!='\n');}
 		else if(n<0){			puts("Has puesto un numero negativo\n");}
-		else{					printf("Tu número es %i\n",n);		}//aqui podria cerrar el cilo haciendo argo=1 y en while argo!=1
+		else{					printf("Tu nÃºmero es %i\n",n);		}//aqui podria cerrar el cilo haciendo argo=1 y en while argo!=1
 	}while(1);
 	return EXIT_SUCCESS;}
