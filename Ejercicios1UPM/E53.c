ec7a78f1e0ba517c500a69bba0d61f52b547abd6
adelpozoman <adelpozoman@gmail.com>
2017-04-25 19:03:05 +0200

    Ordenar vectores lo mas dificil
    O creo otro vector y voy metiendo los valores ordenados (muchas formas pero dificil) o copio el vector y voy ordenando la primera posicion e intercalo, luego en todo el vector menos la primera posicion y asi (más fácil y como lo he hecho)

--- Ejercicios1UPM/E53.c
+++ Ejercicios1UPM/E53.c
@@ -7,7 +7,9 @@
 
 
 int main(){
+while(1){
 	int dim;
+	puts("");
 	puts("Introduce la dimensión del vector");
 	scanf("%i",&dim);
 	int vector[dim];
@@ -26,7 +28,7 @@ int main(){
 	}while(contador<dim);
 	norma=sqrt(norma);
 	printf("La norma 2 (modulo) es %f\n",norma);
-//-----------------------
+//----------------VAMOS A SUMAR EL VALOR ABSOLUTO DE LAS COMPONENTES
 	contador=0;
 	int suma=0;
 	do{
@@ -34,16 +36,16 @@ int main(){
 		contador++;	
 	}while(contador<dim);
 	printf("La norma 1 (suma de los módulos) es: %i\n",suma);
-//---------------------------------------
+//-----------AHORA HAY QUE VER EL COMPONENTE EN ABSOLUTO MÁS GRANDE
 	contador=0;
 	int max=abs(vector[contador]);
 	do{
-		if(max>abs(vector[contador])){
+		if(max<abs(vector[contador])){
 			max=abs(vector[contador]);}
 		contador++;
-	}while(contador<0);
+	}while(contador<dim);
 	printf("La norma máxima es %i\n",max);
-//-------------------
+//-----------AHORA HAY QUE INVERTIR EL VECTOR
 	int invertida[dim];
 	contador=0;
 	do{
@@ -56,26 +58,33 @@ int main(){
 		printf("Vector invertido[%i]=%i\n",contador, invertida[contador]);
 		contador++;
 	}while(contador<dim);
-//--------------
-	int ordenada[dim], intento, posordenada=0;
-	int min, aux;
-	//codigo ordenar vector aqui
+//-------------ahora hay que ordenar el vector
+//-------------------------------------
+	int ordenado[dim];
+	contador=0;
 	do{
-		
-		
-		
-		
+		ordenado[contador]=vector[contador];
+		contador++;
+	}while(contador<dim);
+	int min, argo, pos,aux;
+	contador=0;
+	do{	argo=contador;
+		min=ordenado[contador];
+		do{	if(ordenado[argo]<=min){min=ordenado[argo];pos=argo;}
+			argo++;
+		}while(argo<dim);
+		aux=ordenado[contador];
+		ordenado[contador]=min;
+		ordenado[pos]=aux;
+		contador++;
+	}while(contador<dim);
 	
 	
-		
-		
-		posordenada++;
-	}while(posordenada<dim);
-//-----------
+//--------------------
 	puts("El vector ordenado es:");
 	contador=0;
 	do{
-		printf("Ordenada[%i]=%i\n",contador,ordenada[contador]);
+		printf("Ordenada[%i]=%i\n",contador,ordenado[contador]);
 		contador++;	
-	}while(contador<dim);
-	return 0;}
+}while(contador<dim);
+}return 0;}
